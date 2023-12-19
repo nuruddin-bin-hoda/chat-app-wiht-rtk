c1970d39c79b511119784188c01abb52d9b3a78f
Nuruddin <nuruddinbinhoda@gmail.com>
2023-12-19 22:19:07 +0600

    add components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,27 @@
-function App() {
-  return (
+import {
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from "react-router-dom";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Conversation from "./pages/Conversation";
+import Inbox from "./pages/Inbox";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
     <>
-      <h1>App</h1>
+      <Route path="/" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/inbox" element={<Conversation />} />
+      <Route path="/inbox/:id" element={<Inbox />} />
     </>
-  );
+  )
+);
+
+function App() {
+  return <RouterProvider router={router} />;
 }
 
 export default App;
