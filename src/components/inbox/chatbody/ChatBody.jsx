ae0fda478f28c4aeffb854934379ede957d890c6
Nuruddin <nuruddinbinhoda@gmail.com>
2023-12-22 16:07:55 +0600

    add: message body feature

--- src/components/inbox/chatbody/ChatBody.jsx
+++ src/components/inbox/chatbody/ChatBody.jsx
@@ -1,20 +1,41 @@
 // import Blank from "./Blank";
+import { useParams } from "react-router-dom";
 import ChatHead from "./ChatHead";
 import Messages from "./Messages";
 import Options from "./Options";
+import { useGetMessagesQuery } from "../../../features/messages/messagesApi";
+import Error from "../../ui/Error";
 
 export default function ChatBody() {
+  const { id } = useParams();
+  const {
+    data: messages,
+    isLoading,
+    isError,
+    isSuccess,
+  } = useGetMessagesQuery(id);
+
+  // decide what to render
+  let content = null;
+
+  if (isLoading) content = <h3>Loading...</h3>;
+
+  if (isError) content = <Error message="Theer was an error!" />;
+
+  if (messages?.length === 0) content = <h3>No messages found!</h3>;
+
+  if (isSuccess && messages?.length > 0)
+    content = (
+      <>
+        <ChatHead message={messages[0]} />
+        <Messages messages={messages} />
+        <Options />
+      </>
+    );
+
   return (
     <div className="w-full lg:col-span-2 lg:block">
-      <div className="w-full grid conversation-row-grid">
-        <ChatHead
-          avatar="https://cdn.pixabay.com/photo/2018/01/15/07/51/woman-3083383__340.jpg"
-          name="Akash Ahmed"
-        />
-        <Messages />
-        <Options />
-        {/* <Blank /> */}
-      </div>
+      <div className="w-full grid conversation-row-grid">{content}</div>
     </div>
   );
 }
--- src/components/inbox/chatbody/ChatHead.jsx
+++ src/components/inbox/chatbody/ChatHead.jsx
@@ -1,13 +1,24 @@
+import { useSelector } from "react-redux";
+import gravatarUrl from "gravatar-url";
+
 /* eslint-disable react/prop-types */
-export default function ChatHead({ avatar, name }) {
+export default function ChatHead({ message }) {
+  const { sender, receiver } = message || {};
+  const {
+    user: { email },
+  } = useSelector((state) => state.auth) || {};
+
+  const partnerEmail = sender.email === email ? receiver.email : sender.email;
+  const partnerName = sender.email === email ? receiver.name : sender.name;
+
   return (
     <div className="relative flex items-center p-3 border-b border-gray-300">
       <img
         className="object-cover w-10 h-10 rounded-full"
-        src={avatar}
-        alt={name}
+        src={gravatarUrl(partnerEmail)}
+        alt={partnerName}
       />
-      <span className="block ml-2 font-bold text-gray-600">{name}</span>
+      <span className="block ml-2 font-bold text-gray-600">{partnerName}</span>
     </div>
   );
 }
