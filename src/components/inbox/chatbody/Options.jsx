46d9fa19a81570aaf3a87b2df038d8095b328901
Nuruddin <nuruddinbinhoda@gmail.com>
2023-12-23 15:19:36 +0600

    edit: inbox message filed

--- src/components/inbox/chatbody/Options.jsx
+++ src/components/inbox/chatbody/Options.jsx
@@ -29,7 +29,9 @@ export default function Options() {
 
   const [editConversation] = useEditConversationMutation();
 
-  const handleSendMessage = () => {
+  const handleSendMessage = (e) => {
+    e.preventDefault();
+
     if (message !== "") {
       editConversation({
         id,
@@ -50,7 +52,11 @@ export default function Options() {
   };
 
   return (
-    <div className="flex items-center justify-between w-full p-3 border-t border-gray-300">
+    <form
+      onSubmit={handleSendMessage}
+      className="flex items-center justify-between w-full p-3 border-t border-gray-300"
+      autoComplete="off"
+    >
       <input
         type="text"
         placeholder="Message"
@@ -60,7 +66,7 @@ export default function Options() {
         value={message}
         onChange={(e) => setMessage(e.target.value)}
       />
-      <button disabled={isLoading} onClick={handleSendMessage} type="submit">
+      <button disabled={isLoading} type="submit">
         <svg
           className="w-5 h-5 text-gray-500 origin-center transform rotate-90"
           xmlns="http://www.w3.org/2000/svg"
@@ -70,6 +76,6 @@ export default function Options() {
           <path d="M10.894 2.553a1 1 0 00-1.788 0l-7 14a1 1 0 001.169 1.409l5-1.429A1 1 0 009 15.571V11a1 1 0 112 0v4.571a1 1 0 00.725.962l5 1.428a1 1 0 001.17-1.408l-7-14z" />
         </svg>
       </button>
-    </div>
+    </form>
   );
 }
